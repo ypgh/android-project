65b1b087f7a2e642f163b2132f589d4067579c19
yupeng <yupeng@trustmobi.com>
2015-09-16 15:29:01 +0800

    good

---
+++ app/src/androidTest/java/com/example/the_new_one/myapplication/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.example.the_new_one.myapplication;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    private String nba ="说不上为什么 我变得很主动";
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
